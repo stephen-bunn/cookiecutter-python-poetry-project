6cfabfe5fb01ea20951f0277f8b0834d663637b5
Stephen Bunn <stephen@bunn.io>
2021-10-05 15:04:43 -0400

    Updating template to default to 3.10.0 and include period for module
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- {{ cookiecutter.project_slug }}/src/{{ cookiecutter.package_name }}/__init__.py
+++ {{ cookiecutter.project_slug }}/src/{{ cookiecutter.package_name }}/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) {{ cookiecutter.year }} {{ cookiecutter.author }} <{{ cookiecutter.email }}>
 # {{ cookiecutter.licenses[cookiecutter.package_license].name }} <{{ cookiecutter.licenses[cookiecutter.package_license].url }}>
 
-"""{{ cookiecutter.title }}
+"""{{ cookiecutter.title }}.
 
 {{ cookiecutter.description }}
 """
